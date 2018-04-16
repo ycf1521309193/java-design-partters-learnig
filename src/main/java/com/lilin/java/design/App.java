f6897fd1d01aa21c549a157d76d54304792c2afe
rockli0724 <819645542@qq.com>
2018-04-16 23:58:12 +0800

    my java-design-pattern learning

---
+++ src/main/java/com/lilin/java/design/App.java
@@ -0,0 +1,13 @@
+package com.lilin.java.design;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
