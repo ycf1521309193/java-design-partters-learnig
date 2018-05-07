b5af033c5a5087969e49d276fcb77233c9909ad5
rockli0724 <Administrator@RockLi>
2018-05-07 16:39:14 +0800

    设计模式归类

---
+++ src/main/java/com/lilin/java/design/creational/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 创建型模式
+ * 共五种：工厂方法模式、抽象工厂模式、单例模式、建造者模式、原型模式。
+ */
+/**
+ * @author Administrator
+ *
+ */
+package com.lilin.java.design.creational;
---
+++ src/main/java/com/lilin/java/design/creational/singleton/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package com.lilin.java.design.creational.singleton;
