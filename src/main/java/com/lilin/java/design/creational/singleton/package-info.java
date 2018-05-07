6e09b866d2f7c88c99a8806f8efcb2ba317c7256
rockli0724 <Administrator@RockLi>
2018-05-07 16:43:38 +0800

    添加说明

--- src/main/java/com/lilin/java/design/creational/singleton/package-info.java
+++ src/main/java/com/lilin/java/design/creational/singleton/package-info.java
@@ -1,8 +1,8 @@
 /**
- * 
- */
-/**
- * @author Administrator
- *
+ * 单例模式
+ * 单例：Singleton
+ * 实例：instance
+ * 同步：synchronized
+ * 类装载器：ClassLoader
  */
 package com.lilin.java.design.creational.singleton;
