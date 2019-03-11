85e3a7a9cb677a189897fdeb320e7ca5f4d4a34f
rockli0724 <819645542@qq.com>
2019-03-11 09:43:31 +0800

    feat：抽象工厂模式

---
+++ src/main/java/com/lilin/java/design/imooc/principle/pattern/creational/abstractfactory/CourseFactory.java
@@ -0,0 +1,11 @@
+package com.lilin.java.design.imooc.principle.pattern.creational.abstractfactory;
+
+/**
+ * @author lilin
+ * @Title: CourseFactory
+ * @date 2019/3/11上午12:24
+ */
+public interface CourseFactory {
+
+
+}
