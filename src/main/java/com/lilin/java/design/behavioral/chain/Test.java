8b322963e8084520c8d24e7c59ca2d312651e49b
rockli0724 <819645542@qq.com>
2019-06-16 16:16:18 +0800

    feat: 评价XXXx

---
+++ src/main/java/com/lilin/java/design/behavioral/chain/Test.java
@@ -0,0 +1,13 @@
+package com.lilin.java.design.behavioral.chain;
+
+/**
+ * @author lilin
+ * @Title: Test
+ * @date 2019/6/16下午4:13
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+    }
+}
